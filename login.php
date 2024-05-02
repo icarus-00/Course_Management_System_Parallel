5e039f53e309711d055306fee6f9f2b61b57e809
Elsayed Osama Ragab Elsayed <102467465+0xkillua@users.noreply.github.com>
2024-05-02 19:12:32 +0300

    add login page

--- login.php
+++ login.php
@@ -2,44 +2,58 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MY ASSIGMENT</title>
-    <link rel="stylesheet" href="../css/login.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <title>Animated Login & Registration Form | Codehal</title>
+    <link rel="stylesheet" type="text/css" href="layout/css/login.css">
 </head>
 <body>
-    <div class="container">
-        <div class="main-div">
-            <div class="title">
-                Login Form
+<div class="wrapper">
+    <div class="form-wrapper sign-in">
+        <form action="">
+            <h2>Login</h2>
+            <div class="input-group">
+                <input type="text" required>
+                <label for="">Username</label>
             </div>
-            <div class="form">
-                <form action="index.html">
-                    <div class="row-1">
-                        <div class="icon">
-                            <span class="material-symbols-outlined">
-                            person
-                            </span>
-                        </div>
-                            <input type="text" name="email"  placeholder="Email or Phone" class="input-box"> <br>
-                    </div>
-                    <div class="row-2" >
-                        <div class="icon">
-                            <span class="material-symbols-outlined">
-                                lock
-                                </span>
-                        </div>
-                        <input type="password" name="password" placeholder="Password" class="input-box"> <br>
-                    </div>
-                <p class="forget">Forget Password?</p>
-                <button class="button">Login</button>
-                 <div class="last-paragraph">
-                    <p class="not-member">Not a member?<a href="sign_up.php" style="color:  #c7aea6;"> sign up</a></p>
-                </div>
-                </form>
+            <div class="input-group">
+                <input type="password" required>
+                <label for="">Password</label>
             </div>
-        </div> 
+            <div class="remember">
+                <label><input type="checkbox"> Remember me</label>
+            </div>
+            <button type="submit">Login</button>
+            <div class="signUp-link">
+                <p>Don't have an account? <a href="#" class="signUpBtn-link">Sign Up</a></p>
+            </div>
+        </form>
     </div>
+    <div class="form-wrapper sign-up">
+        <form action="">
+            <h2>Sign Up</h2>
+            <div class="input-group">
+                <input type="text" required>
+                <label for="">Username</label>
+            </div>
+            <div class="input-group">
+                <input type="email" required>
+                <label for="">Email</label>
+            </div>
+            <div class="input-group">
+                <input type="password" required>
+                <label for="">Password</label>
+            </div>
+            <div class="remember">
+                <label><input type="checkbox"> I agree to the terms & conditions</label>
+            </div>
+            <button type="submit">Sign Up</button>
+            <div class="signUp-link">
+                <p>Already have an account? <a href="#" class="signInBtn-link">Sign In</a></p>
+            </div>
+        </form>
+    </div>
+</div>
+<script src="layout/js/login.js"></script>
 </body>
 </html>
