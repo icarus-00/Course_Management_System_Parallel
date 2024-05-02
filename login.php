c6f927aa19e814dddcf5c93ff8ef84895284360c
Icarus'Rise <147264641+icarus-00@users.noreply.github.com>
2024-05-02 14:43:27 +0000

    many changes, added the page folder which contains some useful pages for us to use, the assets contain assets included with the template that had the pages folder, and login page from eman , signup page from maryem

--- login.php
+++ login.php
@@ -1,1 +1,45 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MY ASSIGMENT</title>
+    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+</head>
+<body>
+    <div class="container">
+        <div class="main-div">
+            <div class="title">
+                Login Form
+            </div>
+            <div class="form">
+                <form action="index.html">
+                    <div class="row-1">
+                        <div class="icon">
+                            <span class="material-symbols-outlined">
+                            person
+                            </span>
+                        </div>
+                            <input type="text" name="email"  placeholder="Email or Phone" class="input-box"> <br>
+                    </div>
+                    <div class="row-2" >
+                        <div class="icon">
+                            <span class="material-symbols-outlined">
+                                lock
+                                </span>
+                        </div>
+                        <input type="password" name="password" placeholder="Password" class="input-box"> <br>
+                    </div>
+                <p class="forget">Forget Password?</p>
+                <button class="button">Login</button>
+                 <div class="last-paragraph">
+                    <p class="not-member">Not a member?<a href="sign_up.php" style="color:  #c7aea6;"> sign up</a></p>
+                </div>
+                </form>
+            </div>
+        </div> 
+    </div>
+</body>
+</html>
